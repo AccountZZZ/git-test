1c9a4c253fb6b9c91ade8bd1f4638275743df980
AccountZZZ <88441216+AccountZZZ@users.noreply.github.com>
2022-01-16 23:37:12 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,6 +1,7 @@
 package com.atguigu.git;
 
 public class GitTest {
+    String j;
     public static void main(String[] args) {
         System.out.println("Hello, git!");
         System.out.println("Hello, git2!");
